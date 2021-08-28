6f828b00028b47b7eadf5f60ab742f07ed54e685
wesenbergg <b.b.j.1998@gmail.com>
2021-08-28 20:05:10 +0300

    yml

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import express, { _static } from 'express'
+const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
-app.use(_static('dist'))
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
