3bc7f7a0d67837e9c4334b5180cbb7c9b02988f1
wesenbergg <b.b.j.1998@gmail.com>
2021-08-28 20:13:14 +0300

    update yml

--- cypress/integration/pokedex_app.js
+++ cypress/integration/pokedex_app.js
@@ -5,9 +5,9 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
-  it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
-    cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  it('single can be opened', function() {
+    cy.contains('venusaur').click()
+    cy.contains('Next')
+    cy.contains('Previous')
   })
 })
